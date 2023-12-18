f1c0db52c3ebb711b471e0cb2ec9b053b4668957
RomanMazuraka <144357987+RomanMazuraka@users.noreply.github.com>
2023-12-18 18:20:39 +0100

    goit-js-hw-05-done

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,12 @@
 const getTotalBalanceByGender = (users, gender) =>
+    
     users
         .filter((user) => user.gender === gender)
-        .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+        
+        .reduce((totalBalance, user) => totalBalance +  user.balance, 0);
 
-const allUsers = [
+
+const totalUsers = [
     {
     name: "Moore Hensley",
     gender: "male",
@@ -41,6 +44,6 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(totalUsers, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(totalUsers, "female")); // 8863
